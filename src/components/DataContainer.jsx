63f228c32ffaec2e69ca1ed81cca93656a70936d
Baruch Phillips <baruchphillips@gmail.com>
2020-03-19 19:24:59 -0500

    added react google maps

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,15 +1,24 @@
 import React from 'react' 
 import CityData from './CityData'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 const DataContainer = (props) => { 
 
     return(
-      <div>
-     <img src={`https://maps.googleapis.com/maps/api/staticmap?center=USA&size=600x300
-      &key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`} alt=''></img>
+      // <div>
+            <Map
+          google={props.google}
+          zoom={5}
+         //  style={mapStyles}
+          initialCenter={{ lat: 39.381266, lng: -97.922211}}>
+             {/* Can make an onClick, to place marker where clicked */}
+                {/* <Marker position={{ lat: 45.00, lng: -122.00}} /> */}
+          </Map>
     
-      Last Updated {props?.data?.lastChecked}
-      {props?.data?.covid19Stats?.map(state => <CityData state={state} /> )}
-   </div>
+   //      {/* Last Updated {props?.data?.lastChecked}
+   //  {props?.data?.covid19Stats?.map(state => <CityData state={state} /> )}
+   //  </div>   */}
     )}
 
-export default DataContainer
+export default GoogleApiWrapper({
+      apiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY
+    })(DataContainer);
