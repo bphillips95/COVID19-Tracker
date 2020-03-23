3a03a855c272e26bc9f50f09ee46d5d77c1c1f92
Baruch Phillips <baruchphillips@gmail.com>
2020-03-23 12:19:42 -0500

    added movable marker with info box

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,19 +1,49 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 import CityData from './CityData'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 const DataContainer = (props) => { 
+  const [markerShown, setMarkerShown] = useState(false)
+  const [markerPosition, setmarkerPosition] = useState(null)
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false)
+  const [activeMarker, setActiveMarker] = useState({})        //Shows the active marker upon click
+  const [selectedPlace, setSelectedPlace] = useState({})     //Shows the infoWindow to the selected place upon a marker
+
+  const onMapClick = (ref, map, e) => {
+    setMarkerShown(true)
+    setmarkerPosition(e.latLng)
+    console.log(e)
+  }
+
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlace(props)
+    setActiveMarker(marker)
+    setShowingInfoWindow(true)
+  };
+
+const onClose = props => {
+  if (showingInfoWindow) {
+    setActiveMarker(null)
+    setShowingInfoWindow(false)
+  }
+};
 
     return(
       // <div>
-            <Map
+            <Map onClick={onMapClick}
           google={props.google}
           zoom={5}
          //  style={mapStyles}
           initialCenter={{ lat: 39.381266, lng: -97.922211}}>
              {/* Can make an onClick, to place marker where clicked */}
-                {/* <Marker position={{ lat: 45.00, lng: -122.00}} /> */}
+                <InfoWindow  marker={activeMarker}
+          visible={showingInfoWindow}
+          onClose={onClose} >
+               <div>
+            <h4>{selectedPlace.name}</h4>
+          </div>
+          </InfoWindow>
+          {markerShown && <Marker position={markerPosition} onClick={onMarkerClick} name={"BP Center"} />}
           </Map>
-    
    //      {/* Last Updated {props?.data?.lastChecked}
    //  {props?.data?.covid19Stats?.map(state => <CityData state={state} /> )}
    //  </div>   */}
