34950bf89026487fa953d06d8dcdd75f438a6934
Baruch Phillips <baruchphillips@gmail.com>
2020-03-28 22:32:47 -0400

    removed router bc spa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-
-// import { Grid, Card } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './components/Home'
 import './App.css';
 
 function App() {
  
   return (
-    <Router>
-    <Switch>
-      <Route path="/" component={Home}/>
-    </Switch>
-    </Router>
+  <React.Fragment>
+    <Home/>
+  </React.Fragment>
   );
 }
 
